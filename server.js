f812ed87d29f8a94d83cdf68d379f3d3137418ad
Manuel Barral <manuba9017@gmail.com>
2020-09-12 13:15:07 -0300

    archivo server.js modificado

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
+//const bodyParser = require("body-parser");
+//const jwt = require("jsonwebtoken");
 const server = express();
 
-const SERVER_KEY = "manuelDelilahResto";
+//const SERVER_KEY = "manuelDelilahResto";
 
 const port = 3000; 
 
