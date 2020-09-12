0d3ee4fcd7fc39627af32479383a5335b73c124b
Manuel Barral <manuba9017@gmail.com>
2020-09-12 12:49:56 -0300

    creacion de server, dependencias y subida de archivo gitignore

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const server = express();
+
+const SERVER_KEY = "manuelDelilahResto";
+
+const port = 3000; 
+
+server.get("/", (req, resp) => {
+    resp.send("Probando");
+})
+
+server.listen(port, ()=> {
+    console.log(`Server listening at http://localhost:${port}`)
+});
