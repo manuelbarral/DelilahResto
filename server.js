a8f650195be52b1e379b7d23a34442365dfe180d
Manuel Barral <manuba9017@gmail.com>
2020-09-12 18:28:42 -0300

    creacion get de productos

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
-//const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const server = express();
-
+const Sequelize = require("sequelize");
+const connection = new Sequelize("mysql://root@localhost:3306/delilah_resto");
 //const SERVER_KEY = "manuelDelilahResto";
 
 const port = 3000; 
 
-server.get("/", (req, resp) => {
-    resp.send("Probando");
-})
-
 server.listen(port, ()=> {
     console.log(`Server listening at http://localhost:${port}`)
 });
+
+server.use(bodyParser.json());
+
+//Endpoints
+//Productos
+server.post("/productos", (req, res) => {
+    connection.query("INSERT INTO productos (nombre, precio) VALUES (?,?)",
+    {replacements: [req.body.nombre, req.body.precio]})
+    .then((resp)=> {
+        res.status(200).json({resp});
+    })
+})
+
+server.get("/productos", (req, res) => {
+    connection.query("SELECT * FROM productos",
+    {type: Sequelize.QueryTypes.SELECT})
+    .then((results)=> {
+        res.status(200).json(results);
+    })
+})
+
+server.get("/productos/:id", (req, res) => {
+    connection.query("SELECT * FROM productos WHERE id = :id",
+    {replacements: {id: req.params.id }, type: connection.QueryTypes.SELECT})
+    .then((results)=> {
+        res.status(200).json(results);
+    })
+})
