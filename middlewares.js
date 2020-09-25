22252c20f3e2edc3203baf382cd6ea85b1f979d0
Manuel Barral <manuba9017@gmail.com>
2020-09-25 17:23:59 -0300

    middlewares necesarios

---
+++ middlewares.js
@@ -0,0 +1,71 @@
+const jwt = require("jsonwebtoken");
+const Sequelize = require("sequelize");
+const connection = new Sequelize("mysql://root@localhost:3306/delilah_resto");
+
+require("dotenv").config();
+
+const SERVER_KEY = process.env.SERVER_KEY;
+
+let verifyToken;
+
+function verifyLogin(req, res, next) {
+    const headerAuthorization = req.headers.authorization;
+    if(headerAuthorization !== undefined) {
+        next();
+    } else {
+        res.status(401).json({ok: "false", res: "Debes iniciar sesión!"});
+    }
+}
+
+function verifyIdProducts(req, res, next) {
+    connection.query("SELECT * FROM products WHERE id = ?", 
+    {replacements: [req.params.id], type: Sequelize.QueryTypes.SELECT}
+    )
+    .then((product) => {
+        if(product !== 0) {
+            next();
+        } else {
+            res.status(404).json({ok: "false", res: "No hay ningún producto registrado con ese id!"});
+        }
+    });
+};
+
+function adminPermission(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    verifyToken = jwt.verify(token, SERVER_KEY);
+    connection.query("SELECT * FROM users WHERE userName = ?", 
+    {replacements: [verifyToken.userName], type: Sequelize.QueryTypes.SELECT})
+    .then((user) => {
+        if(user[0].admin === "true") {
+            next();
+        } else {
+            res.status(401).json({ok: "false", res: "No cuentas con los permisos necesarios para realizar esta operación!"});
+        }
+    });
+};
+
+function validateInfoProduct(req, res, next) {
+    const {name, price} = req.body;
+    if(!name || !price) {
+        res.status(400).json({ok: "false", res: "Ingresar todos los datos para crear un nuevo producto"});
+    } else {
+        next();
+    }
+};
+
+function validateInfoUser(req, res, next) {
+    const {userName, password, name, lastname, email, telephone, address} = req.body;
+    if(!userName || !password || !name || !lastname || !email || !telephone || !address) {
+        res.status(400).json({ok: "false", res: "Completar todos los datos para crear la cuenta"});
+    } else {
+        next();
+    }
+};
+
+module.exports = {
+    verifyLogin,
+    adminPermission,
+    validateInfoProduct,
+    verifyIdProducts,
+    validateInfoUser
+}
