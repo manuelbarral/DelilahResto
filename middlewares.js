d43cff63f29cadc51d51626a44429e0e795aa908
Manuel Barral <manuba9017@gmail.com>
2020-09-26 20:12:35 -0300

    middlewares necesarios

--- middlewares.js
+++ middlewares.js
@@ -13,19 +13,19 @@ function verifyLogin(req, res, next) {
     if(headerAuthorization !== undefined) {
         next();
     } else {
-        res.status(401).json({ok: "false", res: "Debes iniciar sesión!"});
+        res.status(401).json("Debes iniciar sesión!");
     }
 }
 
 function verifyIdProducts(req, res, next) {
     connection.query("SELECT * FROM products WHERE id = ?", 
-    {replacements: [req.params.id], type: Sequelize.QueryTypes.SELECT}
+    {replacements: [req.params.id], type: connection.QueryTypes.SELECT}
     )
     .then((product) => {
         if(product !== 0) {
             next();
         } else {
-            res.status(404).json({ok: "false", res: "No hay ningún producto registrado con ese id!"});
+            res.status(404).json("No hay ningún producto registrado con ese id!");
         }
     });
 };
@@ -34,12 +34,12 @@ function adminPermission(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     verifyToken = jwt.verify(token, SERVER_KEY);
     connection.query("SELECT * FROM users WHERE userName = ?", 
-    {replacements: [verifyToken.userName], type: Sequelize.QueryTypes.SELECT})
+    {replacements: [verifyToken.userName], type: connection.QueryTypes.SELECT})
     .then((user) => {
-        if(user[0].admin === "true") {
+        if(user[0].admin === 1) {
             next();
         } else {
-            res.status(401).json({ok: "false", res: "No cuentas con los permisos necesarios para realizar esta operación!"});
+            res.status(401).json("No cuentas con los permisos necesarios para realizar esta operación!");
         }
     });
 };
@@ -47,7 +47,7 @@ function adminPermission(req, res, next) {
 function validateInfoProduct(req, res, next) {
     const {name, price} = req.body;
     if(!name || !price) {
-        res.status(400).json({ok: "false", res: "Ingresar todos los datos para crear un nuevo producto"});
+        res.status(400).json("Ingresar todos los datos para crear un nuevo producto");
     } else {
         next();
     }
@@ -56,7 +56,7 @@ function validateInfoProduct(req, res, next) {
 function validateInfoUser(req, res, next) {
     const {userName, password, name, lastname, email, telephone, address, admin} = req.body;
     if(!userName || !password || !name || !lastname || !email || !telephone || !address || !admin) {
-        res.status(400).json({ok: "false", res: "Completar todos los datos para crear la cuenta"});
+        res.status(400).json("Completar todos los datos para crear la cuenta");
     } else {
         next();
     }
@@ -64,24 +64,24 @@ function validateInfoUser(req, res, next) {
 
 function validateUserPassword(req, res, next) {
     connection.query("SELECT * FROM users WHERE userName = ?",
-    {replacements: [req.body.userName], type: Sequelize.QueryTypes.SELECT})
+    {replacements: [req.body.userName], type: connection.QueryTypes.SELECT})
     .then((user) => {
-        if(user.password === req.body.password) {
+        if(user[0].password === req.body.password) {
             next();
         } else {
-            res.status(401).json({ok: "false", res: "Usuario o contraseña incorrecta"});
+            res.status(401).json("Usuario o contraseña incorrecta");
         }
     });
 };
 
 function verifyIdOrders(req, res, next) {
     connection.query("SELECT * FROM orders WHERE id = ?",
-    {replacements: [req.params.id], type: Sequelize.QueryTypes.SELECT})
+    {replacements: [req.params.id], type: connection.QueryTypes.SELECT})
     .then((order) => {
         if(order !== 0) {
             next();
         } else {
-            res.status(404).json({ok: "false", res: "No existe pedido con ese id"});
+            res.status(404).json("No existe pedido con ese id");
         }
     });
 };
@@ -89,7 +89,7 @@ function verifyIdOrders(req, res, next) {
 function validateInfoOrder(req, res, next) {
     const {id, name, price, paymentMethod, address} = req.body;
     if(!id || !name || !price || !paymentMethod || !address) {
-        res.status(400).json({ok: "false", res: "Ingresar todos los datos para crear un nuevo pedido"});
+        res.status(400).json("Ingresar todos los datos para crear un nuevo pedido");
     } else {
         next();
     }
