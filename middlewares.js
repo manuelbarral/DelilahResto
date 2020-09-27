8f8e8b855bffa75429c5b8c2916fc7a7c44c0bb1
Manuel Barral <manuba9017@gmail.com>
2020-09-27 00:00:12 -0300

    middlewares necesarios

--- middlewares.js
+++ middlewares.js
@@ -1,107 +1,150 @@
-const jwt = require("jsonwebtoken");
-const Sequelize = require("sequelize");
-const connection = new Sequelize("mysql://root@localhost:3306/delilah_resto");
+const jwt = require('jsonwebtoken');
+const Sequelize = require('sequelize');
+const connection = new Sequelize(
+	'mysql://root@localhost:3306/delilah_resto'
+);
 
-require("dotenv").config();
+require('dotenv').config();
 
 const SERVER_KEY = process.env.SERVER_KEY;
 
 let verifyToken;
 
 function verifyLogin(req, res, next) {
-    const headerAuthorization = req.headers.authorization;
-    if(headerAuthorization !== undefined) {
-        next();
-    } else {
-        res.status(401).json("Debes iniciar sesión!");
-    }
+	const headerAuthorization = req.headers.authorization;
+	if (headerAuthorization !== undefined) {
+		next();
+	} else {
+		res.status(401).json('Debes iniciar sesión!');
+	}
 }
 
 function verifyIdProducts(req, res, next) {
-    connection.query("SELECT * FROM products WHERE id = ?", 
-    {replacements: [req.params.id], type: connection.QueryTypes.SELECT}
-    )
-    .then((product) => {
-        if(product !== 0) {
-            next();
-        } else {
-            res.status(404).json("No hay ningún producto registrado con ese id!");
-        }
-    });
-};
+	connection
+		.query('SELECT * FROM products WHERE id = ?', {
+			replacements: [req.params.id],
+			type: connection.QueryTypes.SELECT,
+		})
+		.then((product) => {
+			if (product[0].id !== 0) {
+				next();
+			} else {
+				res
+					.status(404)
+					.json('No hay ningún producto registrado con ese id!');
+			}
+		});
+}
 
 function adminPermission(req, res, next) {
-    const token = req.headers.authorization.split(" ")[1];
-    verifyToken = jwt.verify(token, SERVER_KEY);
-    connection.query("SELECT * FROM users WHERE userName = ?", 
-    {replacements: [verifyToken.userName], type: connection.QueryTypes.SELECT})
-    .then((user) => {
-        if(user[0].admin === 1) {
-            next();
-        } else {
-            res.status(401).json("No cuentas con los permisos necesarios para realizar esta operación!");
-        }
-    });
-};
+	const token = req.headers.authorization.split(' ')[1];
+	verifyToken = jwt.verify(token, SERVER_KEY);
+	connection
+		.query('SELECT * FROM users WHERE userName = ?', {
+			replacements: [verifyToken.userName],
+			type: connection.QueryTypes.SELECT,
+		})
+		.then((user) => {
+			if (user[0].admin === 'true') {
+				next();
+			} else {
+				res
+					.status(401)
+					.json(
+						'No cuentas con los permisos necesarios para realizar esta operación!'
+					);
+			}
+		});
+}
 
 function validateInfoProduct(req, res, next) {
-    const {name, price} = req.body;
-    if(!name || !price) {
-        res.status(400).json("Ingresar todos los datos para crear un nuevo producto");
-    } else {
-        next();
-    }
-};
+	const {name, price} = req.body;
+	if (!name || !price) {
+		res
+			.status(400)
+			.json('Ingresar todos los datos para crear un nuevo producto');
+	} else {
+		next();
+	}
+}
 
 function validateInfoUser(req, res, next) {
-    const {userName, password, name, lastname, email, telephone, address, admin} = req.body;
-    if(!userName || !password || !name || !lastname || !email || !telephone || !address || !admin) {
-        res.status(400).json("Completar todos los datos para crear la cuenta");
-    } else {
-        next();
-    }
-};
+	const {
+		userName,
+		password,
+		name,
+		lastname,
+		email,
+		telephone,
+		address,
+		admin,
+	} = req.body;
+	if (
+		!userName ||
+		!password ||
+		!name ||
+		!lastname ||
+		!email ||
+		!telephone ||
+		!address ||
+		!admin
+	) {
+		res
+			.status(400)
+			.json('Completar todos los datos para crear la cuenta');
+	} else {
+		next();
+	}
+}
 
 function validateUserPassword(req, res, next) {
-    connection.query("SELECT * FROM users WHERE userName = ?",
-    {replacements: [req.body.userName], type: connection.QueryTypes.SELECT})
-    .then((user) => {
-        if(user[0].password === req.body.password) {
-            next();
-        } else {
-            res.status(401).json("Usuario o contraseña incorrecta");
-        }
-    });
-};
+	connection
+		.query('SELECT * FROM users WHERE userName = ?', {
+			replacements: [req.body.userName],
+			type: connection.QueryTypes.SELECT,
+		})
+		.then((user) => {
+			if (user[0].password === req.body.password) {
+				next();
+			} else {
+				res.status(401).json('Usuario o contraseña incorrecta');
+			}
+		});
+}
 
 function verifyIdOrders(req, res, next) {
-    connection.query("SELECT * FROM orders WHERE id = ?",
-    {replacements: [req.params.id], type: connection.QueryTypes.SELECT})
-    .then((order) => {
-        if(order !== 0) {
-            next();
-        } else {
-            res.status(404).json("No existe pedido con ese id");
-        }
-    });
-};
+	connection
+		.query('SELECT * FROM orders WHERE id = ?', {
+			replacements: [req.params.id],
+			type: connection.QueryTypes.SELECT,
+		})
+		.then((order) => {
+			if (order !== 0) {
+				next();
+			} else {
+				res.status(404).json('No existe pedido con ese id');
+			}
+		});
+}
 
 function validateInfoOrder(req, res, next) {
-    const {id, name, price, paymentMethod, address} = req.body;
-    if(!id || !name || !price || !paymentMethod || !address) {
-        res.status(400).json("Ingresar todos los datos para crear un nuevo pedido");
-    } else {
-        next();
-    }
-};
+	const {id, name, price, paymentMethod, address} = req.body;
+	if (!id || !name || !price || !paymentMethod || !address) {
+		res
+			.status(400)
+			.json('Ingresar todos los datos para crear un nuevo pedido');
+	} else {
+		next();
+	}
+}
 
 module.exports = {
-    verifyLogin,
-    adminPermission,
-    validateInfoProduct,
-    verifyIdProducts,
-    validateInfoUser,
-    validateUserPassword,
-    verifyIdOrders,
-    validateInfoOrder
-}
+	verifyLogin,
+	adminPermission,
+	validateInfoProduct,
+	verifyIdProducts,
+	validateInfoUser,
+	validateUserPassword,
+	verifyIdOrders,
+	validateInfoOrder,
+};
